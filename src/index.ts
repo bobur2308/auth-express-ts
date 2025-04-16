295cf7a39577e6fb969234db19f93d9fde848898
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2025-04-16 18:31:20 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express, { Request, Response } from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db'
+import authRouter from './routes/auth.router'
+import cookieParser from 'cookie-parser'
 
 dotenv.config()
 const app = express()
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 connectDB()
 
 //routes
+app.use('/api/auth',authRouter)
 
 
 const port = process.env.port || 5000
