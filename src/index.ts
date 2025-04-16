a87f34a7fa876389be1f6974deb41359c76b5953
Bobur Niyozov <bobur.niyazov0200@gmail.com>
2025-04-16 09:42:16 +0500

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express'
+import dotenv from 'dotenv'
+import connectDB from './config/db'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+connectDB()
+
+//routes
+
+
+const port = process.env.port || 5000
+app.listen(port,()=>{
+  console.log(`Server is running on port ${port}`)
+})
